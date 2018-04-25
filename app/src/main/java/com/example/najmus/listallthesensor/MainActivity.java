031f29715856a4d67def72595e182bdc6ed337c2
NAJMUS SEEMAB <najmus.seemab@gmail.co>
2018-04-25 16:02:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/najmus/listallthesensor/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.najmus.listallthesensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+        SensorManager sensorManager = (SensorManager) getSystemService ( Context.SENSOR_SERVICE );
+        List<Sensor> sensors = sensorManager.getSensorList (  )
+    }
+}
